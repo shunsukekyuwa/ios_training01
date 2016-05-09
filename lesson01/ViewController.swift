ccc0ec22b53e1317bd5ce1638eb3e534ab924a90
Soya Tanaka <soyanchu.me@gmail.com>
2016-05-09 10:19:51 +0900

    Initial Commit

---
+++ lesson01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lesson01
+//
+//  Created by Soya Tanaka on 2016/05/09.
+//  Copyright © 2016年 Soya Tanaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
